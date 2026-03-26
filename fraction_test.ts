cb64f0ea90565391edbe40b31ce030e9cd9da7c8
zoe <Zoe_Bachmann@sluz.ch>
2026-03-26 20:34:16 +0100

    Update fraction_test.ts

--- fraction_test.ts
+++ fraction_test.ts
@@ -34,3 +34,52 @@ Deno.test("1/3 + 2/6 = 2/3 is roughly 0.67", () => {
   // Assert
   assertAlmostEquals(left.toFloat(0.01), 0.67);
 });
+
+
+Deno.test("1/2 - 1/4 = 1/4 is roughly 0.25", () => {
+  //Arrange 
+  const left = new Fraction(1, 2);
+  const right = new  Fraction(1 , 4 );
+   // Acht 
+   left.subtract(right);
+
+   //Assert
+   assertAlmostEquals(left.toFloat(0.001), 0.25);
+});
+
+Deno.test("1/2 * 1/2 = 1/4 or 0.25", () => {
+// Arrange
+const left = new Fraction(1, 2);
+const right = new  Fraction(1 , 2 );
+//Acht
+left.multiply(right);
+//Assert
+assertAlmostEquals(left.toFloat(0.001),0.25);
+});
+
+
+Deno.test("1/2 : 1/2 = 1/4 or 0.25", () => {
+// Arrange
+const left = new Fraction(1, 2);
+const right = new  Fraction(1 , 2 );
+//Acht
+left.divide(right);
+//Assert
+assertAlmostEquals(left.toFloat(0.001),1.0);
+});
+
+Deno.test("ToString  metoden",() => {
+
+//Arrange
+const f = new Fraction(3, 4);
+//Acht
+const actual = f.toString();
+// Assert
+ assertEquals(actual, "3/4"); });
+
+ Deno.test("parse valid fraction string", () => {
+  const f = Fraction.parse("3/4");
+
+  assertEquals(f.toString(), "3/4");
+});
+
